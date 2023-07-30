119de89afbd7e089ddc58c0198ae371442c87f7e
chengcheng <1745829751@qq.com>
2023-07-30 20:06:42 +0800

    master commit2

--- src/main/java/com/cjc/git/Test.java
+++ src/main/java/com/cjc/git/Test.java
@@ -4,5 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好!git");
         System.out.println("你好!git2");
+        System.out.println("你好!git2");
+        System.out.println("你好!git2");
+        System.out.println("你好!git2");
     }
 }

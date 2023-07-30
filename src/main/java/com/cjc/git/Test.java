16d506173ab647f4f0a0e0e88f4df439c7df06a9
chengcheng <1745829751@qq.com>
2023-07-30 19:42:13 +0800

    second commit

--- src/main/java/com/cjc/git/Test.java
+++ src/main/java/com/cjc/git/Test.java
@@ -3,6 +3,6 @@ package com.cjc.git;
 public class Test {
     public static void main(String[] args) {
         System.out.println("你好!git");
-        System.out.println("你好!git");
+        System.out.println("你好!git2");
     }
 }
